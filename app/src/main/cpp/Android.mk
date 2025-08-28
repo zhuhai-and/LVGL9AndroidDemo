563d7b38b37b0e0764d7dda7a53a685606620992
huzongyao <huzongyao@tangbull.com>
2025-08-28 16:47:24 +0800

    updeta lv 9.3.0

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LV_PATH := $(LOCAL_PATH)/lvgl-9.2.0/
+LV_PATH := $(LOCAL_PATH)/lvgl-9.3.0/
 
 LOCAL_SHORT_COMMANDS := true
 LOCAL_ARM_NEON := true
@@ -18,6 +18,7 @@ LOCAL_SRC_FILES := \
 	$(wildcard $(LV_PATH)/src/**/*.c) \
 	$(wildcard $(LV_PATH)/src/**/**/*.c) \
 	$(wildcard $(LV_PATH)/src/extra/**/**/*.c) \
+	$(wildcard $(LV_PATH)/src/misc/**/**/*.c) \
 	$(wildcard $(LV_PATH)/src/draw/**/**/*.c) \
 	$(wildcard $(LV_PATH)/demos/**/*.c) \
 	$(wildcard $(LV_PATH)/demos/**/**/*.c) \
@@ -25,6 +26,7 @@ LOCAL_SRC_FILES := \
 	$(wildcard $(LOCAL_PATH)/*.cpp)
 
 LOCAL_CFLAGS += -ffunction-sections -fdata-sections -fvisibility=hidden
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 LOCAL_LDFLAGS += -Wl,--gc-sections
 LOCAL_LDLIBS := -llog -landroid
 
